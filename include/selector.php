9c832b17b9bdb2f64a02dac88f55c9397b2a5a36
Everett Toews <everett.toews@gmail.com>
2018-07-15 12:12:31 +1200

    Switch default to cats

--- include/selector.php
+++ include/selector.php
@@ -1,6 +1,6 @@
 <?php
 
-$ini_file = '/etc/config/cotd.properties';  
+$ini_file = '/etc/config/cotd.properties';
 
 $dbv2 = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbv3 = getenv('DBHOST');
@@ -33,21 +33,21 @@ if ( empty($dbhost) ) {
 }
 
 $selector = getenv('SELECTOR');
-// if theme not set by client then determine active theme default to pets
-if ( empty($_SESSION['SELECTOR']) ) { 
+// if theme not set by client then determine active theme default to cats
+if ( empty($_SESSION['SELECTOR']) ) {
 
     if ( !empty($selector) ) {
         $_SESSION['SELECTOR'] = getenv('SELECTOR');
     } elseif ( file_exists($ini_file) ) {
-        $ini_array = parse_ini_file($ini_file);     
+        $ini_array = parse_ini_file($ini_file);
         $_SESSION['SELECTOR'] = $ini_array['selector'];
     } elseif ( $_SESSION['V2'] == 'true' )  {
-        $_SESSION['SELECTOR'] = 'pets';
+        $_SESSION['SELECTOR'] = 'cats';
     } else {
-        $_SESSION['SELECTOR'] = 'pets';
+        $_SESSION['SELECTOR'] = 'cats';
     }
 
-}                                     
+}
 
 if ( !empty($_SESSION['service']) ) {
     $service = $_SESSION['service'];
@@ -63,9 +63,9 @@ if ( !empty($service) ) {
 } elseif ( file_exists('data/'.$_SESSION['SELECTOR'].'.php') ) {
     include('data/'.$_SESSION['SELECTOR'].'.php');
 } else {
-    // Unsupported theme so default to pets
-    $_SESSION['SELECTOR'] = 'pets';
-    include('data/pets.php');
+    // Unsupported theme so default to cats
+    $_SESSION['SELECTOR'] = 'cats';
+    include('data/cats.php');
 }
 
 // Set up page next and prev
